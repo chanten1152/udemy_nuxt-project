3d570997b972a830896f8c0ed9df3fe54ca27e01
chanten1152 <chanten811@gmail.com>
2022-06-23 22:27:58 +0900

    セクション2 22

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="container">
+    <h1>Hello, I am a first Nuxt/Vue app!</h1>
+    <nuxt-link to="/users">Users</nuxt-link>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
+
+<style>
+
+.container{
+  margin: 0 auto;
+  max-width: 1200px;
+  text-align: center;
+}
+
+
+</style>
