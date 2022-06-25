1c29c7c4e2737e127f5111176ce1ac87d415206f
chanten1152 <chanten811@gmail.com>
2022-06-25 22:59:10 +0900

    wip

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>This is absolutely fantastic "about me" page!</h1>
+    <p>test text tetetetetetet</p>
+    <p>test text tetetetetetet</p>
+    <p>test text tetetetetetet</p>
+    <p>test text tetetetetetet</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page{
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/posts/_id/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="single-post-page">
+    <section class="post">
+      <h1>Title of the post</h1>
+      <h1>ここにタイトルが表示される。</h1>
+      <div class="post-details">
+        <div>Last updated on XXX</div>
+        <div>written on NAME</div>
+      </div>
+      <p>Content of the post</p>
+    </section>
+    <section class="post-feedback">
+      <p>let me know what you think about the post, send a mail to <a href="mailto:aaaa.com">feedback@my-domain.com</a> </p>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.single-post-page{
+  margin: 0 auto;
+  max-width: 1200px;
+  text-align: center;
+}
+.post-details{
+  font-size: 30px;
+  color: green;
+}
+</style>
